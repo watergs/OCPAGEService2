d18a1a554ccf1e5ce782f88d473827e8cc705607
Karan Thakker <karan17thakkar@gmail.com>
2024-08-02 10:57:36 +0800

    Change names

--- src/main/java/com/example/demo/controller/BalanceController.java
+++ src/main/java/com/example/demo/controller/BalanceController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/dge/balances")
+@RequestMapping("/age/balances")
 public class BalanceController {
 
     @Autowired
